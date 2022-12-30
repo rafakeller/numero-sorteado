5f2448e1cedb0995a6c2a61a2ff66221611cdca8
rafaela <you@example.com>
2022-12-30 17:57:27 -0300

    Design concluido

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 "use strict"
+
+const quantidade = document.getElementById("input-quantidade");
+
+quantidade.addEventListener("focusout", ()=>{
+  console.log()
+  const numeros = quantidade.value
+})
+
